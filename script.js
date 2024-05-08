fd8bddcbfc1e837010e2906c6d9cdc77d3878681
Rodrigo Gomes <rodrigo.gmssouza@gmail.com>
2024-05-08 22:33:50 +0000

    dropdownmenu

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var dropdownContent = document.getElementById('dropdown-content');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+}
